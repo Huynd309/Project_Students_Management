d62c4c308e118969be110b7d92f246da8bfe53d2
Huynd309 <ngohuy3092005@gmail.com>
2025-11-30 17:50:15 +0700

    Debug DB

--- db_config.php
+++ db_config.php
@@ -1,38 +1,27 @@
 <?php
-$database_url = getenv('DATABASE_URL');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-if (!$database_url) {
-    $host = '127.0.0.1';
-    $port = '5432';
-    $dbname = 'Student_Information';
-    $user = 'postgres';
-    $password = 'Ngohuy3092005';
-    
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
-} else {
-    $db = parse_url($database_url);
-    
-    $host = $db['host'];
-    
-    $port = isset($db['port']) ? $db['port'] : '5432'; 
-    
-    $dbname = ltrim($db['path'], '/');
-    $user = $db['user'];
-    $password = $db['pass'];
-    
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
-}
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$dbname = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+
+echo "<pre>";
+echo "HOST = $host\n";
+echo "PORT = $port\n";
+echo "DB = $dbname\n";
+echo "USER = $user\n";
+echo "</pre>";
 
 try {
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
     $conn = new PDO($dsn, $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Kết nối thành công!";
 } catch (PDOException $e) {
-    if ($database_url) {
-        echo "<h1>Lỗi Kết Nối Server!</h1>";
-        echo "Host: " . htmlspecialchars($host) . "<br>";
-        echo "Port: " . htmlspecialchars($port) . "<br>";
-        echo "DB: " . htmlspecialchars($dbname) . "<br>";
-    }
-    die("Chi tiết lỗi: " . $e->getMessage());
+    echo "Lỗi kết nối: " . $e->getMessage();
 }
 ?>
