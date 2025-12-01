bc159b778296320ab5eb171a7be5a3fa86b4d52d
Huynd309 <ngohuy3092005@gmail.com>
2025-12-01 13:21:31 +0700

    Add smart db_config for both Local and Render

--- db_config.php
+++ db_config.php
@@ -1,19 +1,35 @@
 <?php
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
-error_reporting(0);
+$database_url = getenv('DATABASE_URL');
 
-$host = getenv('DB_HOST');
-$port = getenv('DB_PORT');
-$dbname = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
+if ($database_url) {
+    $db = parse_url($database_url);
+    
+    $host = $db['host'];
+    $port = isset($db['port']) ? $db['port'] : '5432';
+    $dbname = ltrim($db['path'], '/'); 
+    $user = $db['user'];
+    $password = $db['pass'];
+    
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+
+} else {
+    $host = '127.0.0.1';
+    $port = '5432';
+    $dbname = 'Student_Information'; 
+    $user = 'postgres';              
+    $password = 'Ngohuy3092005';     
+    
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+}
 
 try {
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
     $conn = new PDO($dsn, $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Lỗi kết nối CSDL.");
+    if (!$database_url) {
+        die("Lỗi kết nối Localhost: " . $e->getMessage());
+    } else {
+        die("Lỗi kết nối Server. Vui lòng kiểm tra lại biến môi trường.");
+    }
 }
 ?>
