091976523942bec9a365ff2260aba4575ee33b1f
Huynd309 <ngohuy3092005@gmail.com>
2025-12-06 18:32:18 +0700

    Fix A-Z

--- daily_report.php
+++ daily_report.php
@@ -108,25 +108,36 @@ try {
         if (!function_exists('convert_vi_to_en')) {
             function convert_vi_to_en($str) {
                 $str = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/", "a", $str);
-                $str = preg_replace("/(đ)/", "d", $str);
-                $str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/", "A", $str);
-                $str = preg_replace("/(Đ)/", "D", $str);
                 $str = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/", "e", $str);
-                $str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/", "E", $str);
                 $str = preg_replace("/(ì|í|ị|ỉ|ĩ)/", "i", $str);
-                $str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/", "I", $str);
                 $str = preg_replace("/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/", "o", $str);
-                $str = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/", "O", $str);
                 $str = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/", "u", $str);
-                $str = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/", "U", $str);
                 $str = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/", "y", $str);
+                $str = preg_replace("/(đ)/", "d", $str);
+                // CHỮ HOA
+                $str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/", "A", $str);
+                $str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/", "E", $str);
+                $str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/", "I", $str);
+                $str = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/", "O", $str);
+                $str = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/", "U", $str);
                 $str = preg_replace("/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/", "Y", $str);
+                $str = preg_replace("/(Đ)/", "D", $str); 
                 return strtolower($str);
             }
         }
+
         usort($processed_data, function($a, $b) {
-            // Sắp xếp đơn giản nếu không có Collator
-            return strcmp($a['ten'], $b['ten']); 
+            $tenA = convert_vi_to_en($a['ten']);
+            $tenB = convert_vi_to_en($b['ten']);
+            
+            $res = strcmp($tenA, $tenB);
+            
+            if ($res === 0) {
+                $hoA = convert_vi_to_en($a['ho_dem']);
+                $hoB = convert_vi_to_en($b['ho_dem']);
+                return strcmp($hoA, $hoB);
+            }
+            return $res;
         });
 
         // Tìm thủ khoa (Chỉ tìm nếu KHÔNG PHẢI LỚP 8 - Vì lớp 8 ko có điểm tích lũy)
